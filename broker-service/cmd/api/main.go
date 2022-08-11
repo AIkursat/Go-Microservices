62b4d2ec7440b423c77dca52d1eadd001b61f0ff
AIKursat <aynurkursat1999@hotmail.com>
2022-08-11 14:31:53 +0300

    Create broker-service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {}
+
+func main() {
+
+	app := Config{}
+
+	log.Println("Starting broker service on port", webPort)
+
+	// define htttp server
+	srv := &http.Server{
+		Addr : fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+
+	}
+    // start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+  
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,9 @@
+module broker-service
+
+go 1.18
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
+)
