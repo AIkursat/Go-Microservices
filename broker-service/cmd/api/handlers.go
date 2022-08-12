e9c459fe8938335f65c0abeec180f597e6f8d2ee
AIKursat <aynurkursat1999@hotmail.com>
2022-08-12 13:23:10 +0300

    Create makeFile and fix the bug on the handlers

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -4,18 +4,12 @@ import (
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	_ = app.writeJSON(w, http.StatusOK, payload)
+	_ = app.writeJSON(w, http.StatusOK, payload) // Ignore the error
 
 }
